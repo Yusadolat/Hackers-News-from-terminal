9c6f0dfcd18a7589cee1f6341aca3f7cef6ed869
Yusuf Adeyemo <yusadolat@gmail.com>
2016-12-21 11:48:54 +0100

    Update

--- hackersnew.py
+++ hackersnew.py
@@ -20,7 +20,7 @@ class Colors():
     def __init__(self):
         self.colors = {"grey":'30',"red":'31', "green":'32',"yellow":'33',"blue":'34', 'magenta':'35','cyan':'36', 'white':'37'}
         self.other = {"bold":'1', 'dark':'2','underline':'4', 'highlight':'7', 'hide':'8'}
-        self.highlights = {"grey":'40','red':41,'green':'42','yellow':'43','blue':'44','magenta':'45','cyan':'46', 'white':'47'}
+        self.highlights = {"grey":'40','red':41,'green':'42','yellow':'43','blue':'43','magenta':'45','cyan':'46', 'white':'47'}
     def color(self, color=None, highlight=None, text=None, other=None):
         if color:
             text = "\033[{0}m{1}".format(self.colors[color], text)
